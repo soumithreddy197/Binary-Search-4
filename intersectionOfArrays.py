115471ae6ac1c3052fc979d4b9bdade49113d210
soumith <soumithr@buffalo.edu>
2021-03-05 15:15:03 -0500

    Finished Task

---
+++ intersectionOfArrays.py
@@ -0,0 +1,41 @@
+﻿#TimeComplexity:O(mlogm+nlogn) 
+#SpaceComplexity: O(1)
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #For reduced space use sorting
+        nums1.sort()
+        nums2.sort()
+        m=len(nums1);n=len(nums2)
+        i=0;j=0;output=[]
+        while(i<m and j<n):
+            if nums1[i]==nums2[j]:
+                output.append(nums1[i])
+                i+=1;j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            else:
+                j+=1
+        return output
+        
+        
+        '''O(N) Space and O(m+n) Time
+        #using Hashmap
+        output=[]
+        m=len(nums1);n=len(nums2) 
+        dict1=Counter(nums1) #creating dict for count
+        for i in nums2:
+            if i in dict1:
+                output.append(i)
+                dict1[i]-=1
+                if dict1[i]==0:
+                    dict1.pop(i)
+        return output        
+        '''
+
+                
+        
+        
+        
---
+++ medianOfSortedArrays.py
@@ -0,0 +1,33 @@
+﻿#TimeComplexity:O(logn) where n is length of smallest array 
+#SpaceComplexity: O(1)
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m=len(nums1);n=len(nums2)
+        if m>n:
+            return self.findMedianSortedArrays(nums2,nums1)
+        low=0;high=m
+        while(low<=high):
+            mid=low+(high-low)//2
+            p1=mid
+            p2=((m+n)//2)-p1
+            l1=nums1[p1-1] if p1>0 else float('-inf')
+            r1=nums1[p1] if p1<m else float('inf')
+            l2=nums2[p2-1] if p2>0 else float('-inf')
+            r2=nums2[p2] if p2<n else float('inf')
+            
+            if l1<=r2 and l2<=r1:
+                #got answer
+                print(l1,l2,r1,r2)
+                if (m+n)%2==0:
+                    return float((max(l1,l2)+min(r1,r2))/2)
+                else:
+                    return min(r1,r2)
+            elif l1>r2:
+                high=mid-1
+            else:
+                low=mid+1
+        
+        
+        
